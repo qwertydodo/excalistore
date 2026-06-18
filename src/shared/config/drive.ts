038efebacc129192692b77ce3defac1502b84962
qwertydodo <qwertydodo93@gmail.com>
2026-06-18 21:39:49 +0200

    chore: remove dead picker type deps and unused DRIVE_SCOPE
    Local .env WXT_PICKER_API_KEY also removed (untracked); rotate the key.

--- src/shared/config/drive.ts
+++ src/shared/config/drive.ts
@@ -1,6 +1,5 @@
 // Central Drive/OAuth constants. The OAuth client id is injected via the
 // manifest (Task 8); these are the API surface constants used by the client.
-export const DRIVE_SCOPE = "https://www.googleapis.com/auth/drive.file";
 export const DRIVE_API = "https://www.googleapis.com/drive/v3";
 export const DRIVE_UPLOAD = "https://www.googleapis.com/upload/drive/v3";
 export const OAUTH_REVOKE = "https://oauth2.googleapis.com/revoke";
